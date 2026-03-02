5f7d6c9736242cd1b9ad0d710f852c3b81a30afc
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-03-02 20:13:33 +0000

    chore: remove compiled build artifacts from version control

--- node-backend/build/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const intentRouter_1 = require("./intentRouter");
-const openaiRouter_1 = require("./openaiRouter");
-exports.default = (app) => {
-    (0, intentRouter_1.setupIntentRoutes)(app);
-    (0, openaiRouter_1.setupOpenAIRoutes)(app);
-};
-//# sourceMappingURL=index.js.map
