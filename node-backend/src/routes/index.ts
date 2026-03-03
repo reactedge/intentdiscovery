51d67da269d08bb154998d74477aeb8c8fb7d72e
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-03-03 10:38:05 +0000

    Resintate CORS in node backend

--- node-backend/src/routes/index.ts
+++ node-backend/src/routes/index.ts
@@ -1,8 +1,6 @@
 import {setupIntentRoutes} from "./intentRouter"
 import {Application} from "express";
-import {setupOpenAIRoutes} from "./openaiRouter";
 
 export default (app: Application) => {
     setupIntentRoutes(app)
-    setupOpenAIRoutes(app)
 }
